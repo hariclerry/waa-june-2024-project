88eb7d3eb99683f8747299701efed2b51c9771c2
hariclerry <clerryha@gmail.com>
2024-07-16 12:19:56 -0500

    fix api request

--- frontend/src/service/eventsAPI.js
+++ frontend/src/service/eventsAPI.js
@@ -1,34 +1,26 @@
-import axios from 'axios';
-import { getTokens } from '../core/setup/token';
-
-const axioInstance = axios.create({
-    baseURL: `http://localhost:8080/api/v1`,
-    headers: {
-        'Authorization': `Bearer ${getTokens().refreshToken}`
-      }
-});
+import apiClient from "../core/setup/axios";
 
 export const getAllEventsApi = async () => {
-  const response = await axioInstance.get("/events");
+  const response = await apiClient.get("/events");
   return response.data;
 };
 
 export const createEventApi = async (data) => {
-    const response = await axioInstance.post("/admins/events", data);
+    const response = await apiClient.post("/admins/events", data);
     return response.data;
   };
 
   export const getEventApi = async (id) => {
-    const response = await axioInstance.get(`/events/${id}`);
+    const response = await apiClient.get(`/events/${id}`);
     return response.data;
   };
 
   export const deleteEventApi = async (id) => {
-    const response = await axioInstance.delete(`/admins/events/${id}`);
+    const response = await apiClient.delete(`/admins/events/${id}`);
     return response.data;
   };
 
   export const updateEventApi = async (data) => {
-    const response = await axioInstance.put(`/admins/events/${data.id}`, data);
+    const response = await apiClient.put(`/admins/events/${data.id}`, data);
     return response.data;
   };
