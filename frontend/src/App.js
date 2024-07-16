424b2a09a5cb8fffcf1f1a752e605a5a53f42027
Suman Adhikari <expertxkid@gmail.com>
2024-07-15 23:04:43 -0500

    feat/students: add profile view and update routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from './features/authentication/login/Login';
 import Register from './features/authentication/register/Register';
 import Students from './features/students/Students';
 import StudentDetail from './features/students/student-detail';
+import UpdateStudentDetail from './features/students/update-student-detail';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/students" element={<Students />} />
       <Route path="/student/:username" element={<StudentDetail />} />
+      <Route path="/profile/update" element={<UpdateStudentDetail />} />
+      <Route path="/profile" element={<StudentDetail />} />
     </Routes>
   );
 }
