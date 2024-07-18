bf04b6a3d808ba1afb828e711c1627ec8b2f4410
Harriet Ayugi <hariclerry@users.noreply.github.com>
2024-07-18 09:31:57 -0500

    Revert "Bugfix/code merge"

--- frontend/src/features/dashboard/dashboard-student.jsx
+++ frontend/src/features/dashboard/dashboard-student.jsx
@@ -1,31 +1,30 @@
 import NavBar from '../../core/component/NavBar';
-import {faCalendarAlt, faComment, faUserGraduate} from '@fortawesome/free-solid-svg-icons';
+import {fa8, faCalendarAlt, faComment, faUserGraduate} from '@fortawesome/free-solid-svg-icons';
 import {Container, Row} from 'react-bootstrap';
 import DashboardCard from '../dashboard-card/dashboard-card';
 
 export default function StudentDashboard() {
-    const options = [
-        {icon: faUserGraduate, title: 'Students', link: '/students'},
-        {icon: faCalendarAlt, title: 'Events', link: '/events'},
-        {icon: faComment, title: 'Discussion', link: '/discussions'},
-        {icon: faCalendarAlt, title: 'Events', link: '/events'},
+  const options = [
+    {icon: faUserGraduate, title: 'Students', link: '/students'},
+    {icon: faCalendarAlt, title: 'Events', link: '/events'},
+<<<<<<< HEAD
+    {icon: faCalendarAlt, title: 'Feedback', link: '/feedbacks'},
+    {icon: faUserGraduate, title: 'Academic Resouces', link: '/academic-resouces'}
+=======
+    {icon: faComment, title: 'Discussion', link: '/discussions'}
+>>>>>>> develop
+  ];
 
-        {icon: faCalendarAlt, title: 'Feedback', link: '/feedbacks'},
-        {icon: faUserGraduate, title: 'Academic Resouces', link: '/academic-resouces'},
-
-        {icon: faComment, title: 'Discussion', link: '/discussions'}
-    ];
-
-    return (
-        <>
-            <NavBar/>
-            <Container className="mt-5">
-                <Row>
-                    {options.map(opt => (
-                        <DashboardCard icon={opt.icon} link={opt.link} title={opt.title}/>
-                    ))}
-                </Row>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <Container className="mt-5">
+        <Row>
+          {options.map(opt => (
+            <DashboardCard icon={opt.icon} link={opt.link} title={opt.title} />
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 }
